d6f3094ce9b6a4a9b7a5c5c3798d0cee04ccf273
vslinux2048 <vslinux2048@gmail.com>
2017-07-05 18:44:58 +0900

    Add sum modul

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include "sum.h"
+
+int sum(int n)
+{
+    result = 0;
+
+    for(int i = 1; i <=n; i++)
+        result += i;
+
+    return result;
+}
---
+++ sum.h
@@ -0,0 +1,4 @@
+#ifndef __SUB_H__
+int sum(int n);
+#endif
+
